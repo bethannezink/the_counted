0fc6d2cbc65e95c6db7197d35d2113ee239f5dd6
bethannezink <bethannezink@gmail.com>
2017-09-06 13:57:58 -0400

    add counted api call and basic data render

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state  = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://thecountedapi.com/api/counted/')
+    .then(results => {
+      return results.json();
+    }).then(data => {
+      data = data.map((info) => {
+        return(
+          <div key={info._id}>
+            <p> {info.name} </p>
+          </div>
+        )
+      })
+      this.setState({data: data});
+    })
+  }
+
   render() {
     return (
       <div>
-        Hello Schmworld!
+        <div>
+          {this.state.data}
         </div>
-      );
+      </div>
+    )
   }
+
 }
 
 export default App;
